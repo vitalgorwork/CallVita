f474e3f7bee2af97dc425f08283e5e15fb4bdb34
Vitaliy Gorpenko <vitalgorwork@gmail.com>
2025-12-31 09:43:56 -0800

    Initial Commit

---
+++ CallVita/CallVitaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CallVitaApp.swift
+//  CallVita
+//
+//  Created by Vitaliy Gorpenko on 12/31/25.
+//
+
+import SwiftUI
+
+@main
+struct CallVitaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CallVita/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CallVita
+//
+//  Created by Vitaliy Gorpenko on 12/31/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
